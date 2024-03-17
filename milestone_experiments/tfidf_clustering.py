fa939abcb9a5fc631030ac826fac0a1806e57fbb
Duygu <duygu.mustafa04@gmail.com>
2024-03-17 13:34:39 +0100

    tfidf clustering with timestamps

--- milestone_experiments/tfidf_clustering.py
+++ milestone_experiments/tfidf_clustering.py
@@ -2,23 +2,31 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
 
 issues_df = pd.read_csv('../resources/processed_issue_titles.csv')
+
 issues = issues_df['processed_title']
 
-vectorizer = TfidfVectorizer(stop_words='english', lowercase=True)
+vectorizer_title = TfidfVectorizer(stop_words='english', lowercase=True)
+tfidf_matrix_title = vectorizer_title.fit_transform(issues)
 
-tfidf_matrix = vectorizer.fit_transform(issues)
+timestamps = issues_df['timestamp']
+timestamps = pd.to_datetime(timestamps)
 
-similarity_matrix = cosine_similarity(tfidf_matrix)
+scaler_timestamp = StandardScaler()
+timestamps_scaled = scaler_timestamp.fit_transform(timestamps.values.reshape(-1, 1))
 
-num_clusters = 7  # Specify the number of clusters
+combined_features = pd.concat([pd.DataFrame(tfidf_matrix_title.toarray()), pd.DataFrame(timestamps_scaled)], axis=1)
+
+similarity_matrix = cosine_similarity(combined_features)
+
+num_clusters = 10  # Specify the number of clusters
 kmeans = KMeans(n_clusters=num_clusters)
 cluster_labels = kmeans.fit_predict(similarity_matrix)
 
 issues_df['cluster'] = cluster_labels
 
-a = 5
 print(issues_df)
 
 
