2b5c45ee540fa65ed2153f936da77c21704ef1c3
Duygu <duygu.mustafa04@gmail.com>
2024-03-27 17:52:40 +0100

    improve text preprocessing

--- commit_activity/processing.py
+++ commit_activity/processing.py
@@ -2,24 +2,46 @@ import string
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+from nltk import download
+import re
 
-def preprocess_text(text: ""):
+# Download necessary NLTK data (run once)
+# download('punkt')
+# download('stopwords')
+# download('wordnet')
+
+def preprocess_text(text, use_stemming=True, additional_stopwords=set(), remove_numbers=True):
+    """Preprocess text data by tokenizing, lowercasing, removing stopwords, punctuation, and special characters."""
+
+    if "remote" in text:
+        bp = 4
+    additional_stopwords = {"merge", "branch", "pull", "remote", "origin/dev", "dev", "remote-tracking", "master"}
     # Lowercasing
     text = text.lower()
-    text = text.replace('merge branch', '')  # Removing common commit message prefix
+
+    if remove_numbers:
+        # Remove tokens that are entirely numeric
+        text = re.sub(r'\b\d+\b', '', text)
 
     # Tokenization
     tokens = word_tokenize(text)
 
     # Removing Punctuation and Special Characters
-    tokens = [token.strip(string.punctuation) for token in tokens]
+    tokens = [token.strip(string.punctuation) for token in tokens if token.strip(string.punctuation)]
 
     # Removing Stopwords
-    stop_words = set(stopwords.words('english'))
-    tokens = [token for token in tokens if token not in stop_words]
+    stop_words = set(stopwords.words('english')).union(additional_stopwords)
+    assert "remote" in stop_words
+    tokens = [token for token in tokens if token not in stop_words and len(token) > 1]
 
-    # Stemming
-    stemmer = PorterStemmer()
-    tokens = [stemmer.stem(token) for token in tokens]
+    if use_stemming:
+        # Stemming
+        stemmer = PorterStemmer()
+        tokens = [stemmer.stem(token) for token in tokens]
+    else:
+        # Lemmatization
+        lemmatizer = WordNetLemmatizer()
+        tokens = [lemmatizer.lemmatize(token) for token in tokens]
 
     return ' '.join(tokens)
